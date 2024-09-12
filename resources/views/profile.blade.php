11fe0792eaf499f7f122a590863d3053c9555714
adillaads <adilla.aulia1203@gmail.com>
2024-09-12 10:04:44 +0700

    tugas 2

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>Profile</title>
+</head>
+<body>
+    <img src="/img/image.png" class="profile-img" width="200px" alt="Profile Image">
+    <div class="profile-info">
+        <div class="info-box"><?= $nama ?></div>
+        <div class="info-box"><?= $kelas ?></div>
+        <div class="info-box"><?= $npm ?></div>
+    </div>
+</body>
+</html>
